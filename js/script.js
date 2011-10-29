28be49459eece9ff86219af438369de833433930
Gabriel Walt <gabriel.walt@gmail.com>
2011-10-29 15:45:51 +0200

    Basic HTML5 drag & drop

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 /* Author: Gabriel Walt */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+jQuery(function ($) {
+    var dragging = null;
+    
+    $('.container').each (function () {
+        var main = $(this)
+            .delegate('[draggable=true]', 'dragstart', function (event) {
+                event.originalEvent.dataTransfer.setData ("text/plain", event.target.textContent);
+                dragging = this;
+            })
+            .bind('dragover', function (event) {
+                main.addClass('over');
+                //event.originalEvent.dataTransfer.dropEffect = 'copy';
+                event.preventDefault();
+                return false;
+            })
+//            .bind('dragenter', function (event) { // for IE
+//                main.addClass('over');
+//                return false;
+//            })
+            .bind('dragleave', function (event) {
+                main.removeClass('over');
+            })
+            .bind('drop', function (event) {
+                main.append(dragging).removeClass('over');
+                event.stopPropagation();
+                return false;
+            });
+    });
+});
